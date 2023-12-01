b7554f674b0eff75f81b4e3093b5dbd994cac5bc
MaryelizabethMac <111523848+MaryelizabethMac@users.noreply.github.com>
2023-12-01 11:25:57 -0500

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,31 +1,4 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const form = document.getElementById("contact-form");
-    const name1Input = document.getElementById("name1");
-    const emailInput = document.getElementById("email");
-    const comment1Input = document.getElementById("comment1");
-    form.addEventListener("submit", function (event) {
-        // Initialize an array to store error messages
-        const errors = [];
-        // Validation for username (minimum length of 6 characters)
-        if (name1Input.value.length < 6) {
-        errors.push("Username must be at least 3 characters long.");
-        }
-        // Validation for email (must be a valid email format)
-        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-        if (!emailRegex.test(emailInput.value)) {
-        errors.push("Please enter a valid email address.");
-        }
-        // Validation for password (minimum length of 20 characters)
-        if (comment1Input.value.length < 20) {
-        errors.push("Comment must be at least 20 characters long.");
-        }
-        // If there are errors, prevent form submission and display them
-        if (errors.length > 0) {
-        event.preventDefault(); // Prevent form submission
-        alert(errors.join("\n")); // Display error messages in an alert
-        }
-    });
-});
+
 
 document.addEventListener("DOMContentLoaded", function () {
     const form = document.getElementById("review-form");
@@ -51,19 +24,19 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 document.addEventListener("DOMContentLoaded", function () {
-    // Social Media Sharing Functions
-    function shareOnFacebook() {
-    // Replace 'your-website-url' with the URL you want to share
-    const urlToShare = 'https://maryelizabethmac.github.io/thewharfrest/index.html';
-    const facebookShareUrl = `https://www.facebook.com/sharer/sharer.php?u=${encodeURIComponent(urlToShare)}`;
-    window.open(facebookShareUrl, 'Share on Facebook', 'width=600,height=400');
+        // Social Media Sharing Functions
+        function shareOnFacebook() {
+        // Replace 'your-website-url' with the URL you want to share
+        const urlToShare = 'https://maryelizabethmac.github.io/thewharfrest/index.html';
+        const facebookShareUrl = `https://www.facebook.com/sharer/sharer.php?u=${encodeURIComponent(urlToShare)}`;
+        window.open(facebookShareUrl, 'Share on Facebook', 'width=600,height=400');
     }
     function shareOnTwitter() {
-    // Replace 'your-website-url' and 'your-text' with your desired URL and text
-    const urlToShare = 'https://maryelizabethmac.github.io/thewharfrest/index.html';
-    const textToShare = 'Look a this restaurant called The Wharf';
-    const twitterShareUrl = `https://twitter.com/intent/tweet?url=${encodeURIComponent(urlToShare)}&text=${encodeURIComponent(textToShare)}`;
-    window.open(twitterShareUrl, 'Share on Twitter', 'width=600,height=400');
+        // Replace 'your-website-url' and 'your-text' with your desired URL and text
+        const urlToShare = 'https://maryelizabethmac.github.io/thewharfrest/index.html';
+        const textToShare = 'Look a this restaurant called The Wharf';
+        const twitterShareUrl = `https://twitter.com/intent/tweet?url=${encodeURIComponent(urlToShare)}&text=${encodeURIComponent(textToShare)}`;
+        window.open(twitterShareUrl, 'Share on Twitter', 'width=600,height=400');
     }
     function shareOnInstagram() {
         // Replace 'your-website-url' with the URL you want to share
